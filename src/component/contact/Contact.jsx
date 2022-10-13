8fb732f5dc127c24c08e7605fdd3e3b8e620e204
sarvesh-pathak <sarveshgeepathak@gmail.com>
2022-10-13 23:00:34 +0530

    adding form component and removing few bugs

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -3,7 +3,21 @@ import './contact.css'
 import{MdEmail} from "react-icons/md"
 import{FaTelegram} from "react-icons/fa"
 import{RiWhatsappFill} from "react-icons/ri"
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_h3b9hxd', 'template_z1v2qsr', form.current, 'Lb-hH447BDEFy7zgM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -21,15 +35,21 @@ const Contact = () => {
             <FaTelegram className='con__op__icon'/>
             <h4>Telegram</h4>
             <h5>Sarvesh Pathak</h5>
-            <a href='https://t.me/917807883034 ' target="_blank" rel="noopener noreferrer">Send a message</a>
+            <a href='https://t.me/917807883034' target="_blank" rel="noopener noreferrer">Send a message</a>
           </article>
           <article className='con__op'>
             <RiWhatsappFill className='con__op__icon'/>
             <h4>WhatsApp</h4>
             <h5>+91 7807883034</h5>
-            <a href='https://api.whatsapp.com/send?phone=+917807883034 target="_blank"'>Send a message</a>
+            <a href='https://wa.me/917807883034' target="_blank" rel="noopener noreferrer">Send a message</a>
           </article>
         </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required/>
+          <textarea type="message" rows="10" placeholder='Your Message' required/>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
