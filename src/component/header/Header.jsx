5de3125b5ad1043bb06a77c53a298cfc6c5d820a
sarvesh-pathak <sarveshgeepathak@gmail.com>
2023-07-28 22:03:57 +0530

    new phota

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from "./CTA"
-import ME from '../../needs/My Movie.mp4'
+import ME from '../../needs/me.jpeg'
 import Socials from './Socials'
 const Header = () => {
   return (
@@ -12,11 +12,8 @@ const Header = () => {
         <h5 className="text-ligth">Software Developer</h5>
         <CTA/>
         <Socials/>
-        <div className='me'>
-        <video controls autoplay>
-        <source src={ME} type="video/mp4"></source>
-        </video>
-
+        <div>
+        <img src={ME} alt="" className='me'/>
         </div>
         <a href='#contact' className='scroll_down'>Scroll down</a>
       </div>
