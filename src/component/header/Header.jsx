a56dc892c0a7c9754d03de5ad163de3c1a6e14aa
sarvesh-pathak <sarveshgeepathak@gmail.com>
2025-06-26 23:42:35 +0530

    Temporarily took down site for update.

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from "./CTA"
-import ME from '../../needs/me.jpeg'
+import ME from '../../needs/My_Movie.gif'
 import Socials from './Socials'
 const Header = () => {
   return (
